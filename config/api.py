6582ce2f743834a478bf9bc7893acd9f47b1792e
zulaili <mfajarpamungkas@gmail.com>
2025-11-02 18:27:08 +0700

    set custom host

--- config/api.py
+++ config/api.py
@@ -1,12 +1,23 @@
+from django.conf import settings
 from ninja import NinjaAPI, Schema
 
 from scalar_django_ninja import ScalarViewer
+
+
+API_SERVER_URL = getattr(settings, "API_SERVER_URL", ".")
+
+servers = (
+    [{"url": ".", "description": "Resolve against the current host"}]
+    if API_SERVER_URL == "."
+    else [{"url": API_SERVER_URL}]
+)
 
 
 api = NinjaAPI(
     title="Pages API",
     version="1.0.0",
     docs=ScalarViewer(title="Pages API Reference"),
+    servers=servers,
 )
 
 
